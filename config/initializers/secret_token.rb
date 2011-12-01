2f4c7f403ef6818718f522ad65f77c4572ec9822
Will Fisher <will@teknofire.net>
2011-12-01 11:30:17 -0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MapCompare::Application.config.secret_token = '6a572af273ac2634d6c133bba8871e9550821a96316bace41349b79183cf4265385805cd90b1af9d05413f80428daec05ce7b4436f7ff63f889a0a5e0527ecdf'
